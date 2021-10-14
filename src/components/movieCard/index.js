87ed50aa2849d78c98fc0529691f86d9b751f4ae
Ernestas Trakys <20092082@mail.wit.ie>
2021-10-14 14:46:39 +0100

    Added routing and live data from API.

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -13,6 +14,7 @@ import StarRateIcon from "@material-ui/icons/StarRate";
 import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/Grid";
 import img from '../../images/film-poster-placeholder.png'
+
 
 const useStyles = makeStyles({
   card: { maxWidth: 345 },
@@ -56,9 +58,11 @@ export default function MovieCard(props) {
         <IconButton aria-label="add to favorites" onClick={null}>
           <FavoriteIcon color="primary" fontSize="large" />
         </IconButton>
-        <Button variant="outlined" size="medium" color="primary">
-          More Info ...
-        </Button>
+        <Link to={`/movies/${movie.id}`}>
+          <Button variant="outlined" size="medium" color="primary">
+            More Info ...
+          </Button>
+        </Link>
       </CardActions>
     </Card>
   );
